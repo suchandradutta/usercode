74e2315463a522896cdda2d8996f5f9518277548
sarkar <>
2011-11-25 13:11:24 +0000

    offlinePrimaryVerticesWithBS

--- VHTauTau/TreeMaker/python/VertexBlock_cfi.py
+++ VHTauTau/TreeMaker/python/VertexBlock_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 vertexBlock = cms.EDAnalyzer("VertexBlock",
   verbosity = cms.int32(0),
-  vertexSrc = cms.InputTag('offlinePrimaryVertices')
+  vertexSrc = cms.InputTag('offlinePrimaryVerticesWithBS')
 )
