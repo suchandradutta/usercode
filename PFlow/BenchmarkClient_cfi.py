ceca7ea9ac126e014a2b6837f338b5e30355deeb
dutta <>
2011-11-27 18:09:04 +0000

    This commit was manufactured by cvs2svn to create branch 'Nov24_2011'.
    Sprout from master 2011-11-27 18:09:03 UTC sarkar 'againstElectronMVA added in TauBlock, removal of low pt ntries in GenParticleBlock'
    Delete:
        PFlow/BenchmarkClient.cc
        PFlow/BenchmarkClient.h
        PFlow/BenchmarkClient_cfi.py
        PFlow/JetBenchmark.cc
        PFlow/JetBenchmark.h
        PFlow/JetBenchmarkAnalyzer.cc
        PFlow/JetBenchmarkAnalyzer.h
        PFlow/JetValidation_cff.py
        PFlow/JetValidation_cfi.py
        PFlow/MetBenchmark.cc
        PFlow/MetBenchmark.h
        PFlow/MetBenchmarkAnalyzer.cc
        PFlow/MetBenchmarkAnalyzer.h
        PFlow/MetValidation_cff.py
        PFlow/MetValidation_cfi.py
        PFlow/PFlowValidationClient_cff.py
        PFlow/pflowDQM_edm_cfg.py
        PFlow/pflowDQM_harvest_cfg.py
        VHTauTau/TreeAnalyzer/DoubleMu_V4.job
        VHTauTau/TreeAnalyzer/Makefile
        VHTauTau/TreeAnalyzer/README.txt
        VHTauTau/TreeAnalyzer/configana.h
        VHTauTau/TreeAnalyzer/first.job
        VHTauTau/TreeAnalyzer/interface/AnaBase.h
        VHTauTau/TreeAnalyzer/interface/AnaUtil.h
        VHTauTau/TreeAnalyzer/interface/LinkDef.h
        VHTauTau/TreeAnalyzer/interface/PhysicsObjects.h
        VHTauTau/TreeAnalyzer/src/AnaBase.cc
        VHTauTau/TreeAnalyzer/src/AnaUtil.cc
        VHTauTau/TreeAnalyzer/src/PhysicsObjects.cc
        VHTauTau/TreeAnalyzer/src/analysis.cc
        VHTauTau/TreeMaker/src/LinkDef.h

--- PFlow/BenchmarkClient_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-benchmarkClient = cms.EDAnalyzer("BenchmarkClient",
-    FolderNames = cms.vstring("PFJet/CompWithGenJet","PFJet/CompWithCaloJet"),
-    HistogramNames = cms.vstring( "delta_et_VS_et_")                                 
-)
