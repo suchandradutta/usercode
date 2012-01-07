ad8668e57bed91eff0994e4397332730105b65fd
sarkar <>
2012-01-07 05:16:32 +0000

    replace rootcint with reflex

---
+++ VHTauTau/TreeMaker/src/classes.h
@@ -0,0 +1,23 @@
+#include "VHTauTau/TreeMaker/interface/PhysicsObjects.h"
+
+namespace {
+  struct dictionary {
+    vhtm::Event             rv1;
+    vhtm::GenEvent          rv2;
+    vhtm::Electron          rv3;
+    vhtm::GenParticle       rv4;
+    vhtm::GenJet            rv5;
+    vhtm::GenMET            rv6;
+    vhtm::MET               rv7;
+    vhtm::Tau               rv8;
+    vhtm::CaloJet           rv9;
+    vhtm::Muon              rva;
+    vhtm::Jet               rvb;
+    vhtm::SuperCluster      rvc;
+    vhtm::Vertex            rvd;
+    vhtm::Trigger           rve;
+    vhtm::Track             rvf;
+    vhtm::Photon            rvg;
+    vhtm::TriggerObject     rvh;
+  };
+}
