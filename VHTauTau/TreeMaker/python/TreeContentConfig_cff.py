6e601c0a1355d61a259c9fddcfe5abfc92d55bd1
sarkar <>
2012-02-20 03:24:07 +0000

    *** empty log message ***

--- VHTauTau/TreeMaker/python/TreeContentConfig_cff.py
+++ VHTauTau/TreeMaker/python/TreeContentConfig_cff.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from VHTauTau.TreeMaker.EventBlock_cfi import eventBlock
 from VHTauTau.TreeMaker.VertexBlock_cfi import vertexBlock
-from VHTauTau.TreeMaker.CaloJetBlock_cfi import caloJetBlock
 from VHTauTau.TreeMaker.JetBlock_cfi import jetBlock
 from VHTauTau.TreeMaker.ElectronBlock_cfi import electronBlock
 from VHTauTau.TreeMaker.METBlock_cfi import metBlock
@@ -11,21 +10,24 @@ from VHTauTau.TreeMaker.TauBlock_cfi import tauBlock
 from VHTauTau.TreeMaker.GenParticleBlock_cfi import genParticleBlock
 from VHTauTau.TreeMaker.GenJetBlock_cfi import genJetBlock
 from VHTauTau.TreeMaker.GenMETBlock_cfi import genMETBlock
+from VHTauTau.TreeMaker.TrackBlock_cfi import trackBlock
 from VHTauTau.TreeMaker.TriggerBlock_cfi import triggerBlock
 from VHTauTau.TreeMaker.PhotonBlock_cfi import photonBlock
+from VHTauTau.TreeMaker.CommonVertexBlock_cfi import commonVertexBlock
 
 treeContentSequence = cms.Sequence(
    eventBlock
  + vertexBlock
- + caloJetBlock
- + jetBlock  
+ + commonVertexBlock
  + electronBlock
- + metBlock
- + muonBlock
- + tauBlock
  + genParticleBlock
  + genJetBlock
  + genMETBlock  
+ + jetBlock  
+ + metBlock
+ + muonBlock
+ + tauBlock
+ + trackBlock
  + triggerBlock
  + photonBlock
 )
