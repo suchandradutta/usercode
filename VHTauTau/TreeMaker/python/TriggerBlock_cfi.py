1c6216067ad343d5b87a5cb02c44398e5fd8bbf8
sarkar <>
2012-02-29 12:56:44 +0000

    More trigger paths added to TriggerBlock

--- VHTauTau/TreeMaker/python/TriggerBlock_cfi.py
+++ VHTauTau/TreeMaker/python/TriggerBlock_cfi.py
@@ -4,6 +4,13 @@ triggerBlock = cms.EDAnalyzer("TriggerBlock",
   verbosity = cms.int32(0),
   l1InputTag  = cms.InputTag('gtDigis'),
   hltInputTag = cms.InputTag('TriggerResults','','HLT'),
-  hltPathsOfInterest = cms.vstring ("HLT_DoubleMu","HLT_Mu","HLT_TripleMu","LooseIsoPFTau",
-                                    "TightIsoPFTau", "DoubleIsoPFTau", "TrkIsoT")
+  hltPathsOfInterest = cms.vstring ("HLT_DoubleMu",
+                                    "HLT_Mu",
+                                    "HLT_IsoMu", 
+                                    "HLT_TripleMu",
+                                    "LooseIsoPFTau",
+                                    "TightIsoPFTau", 
+                                    "DoubleIsoPFTau", 
+                                    "TrkIsoT", 
+                                    "HLT_Ele")
 )
