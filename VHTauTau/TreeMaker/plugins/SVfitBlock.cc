5601d9a68bec18b5aac27d9387ca019845b12797
sarkar <>
2012-05-15 17:14:01 +0000

    SVfit updated

--- VHTauTau/TreeMaker/plugins/SVfitBlock.cc
+++ VHTauTau/TreeMaker/plugins/SVfitBlock.cc
@@ -39,6 +39,10 @@ void SVfitBlock::beginJob()
 }
 void SVfitBlock::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  // Reset the TClonesArray and the nObj variables
+  cloneSVDiTau->Clear();
+  fnSVDiTau = 0;
+
   edm::Handle<NSVfitEventHypothesisByIntegrationCollection> tauPairsInt;
   iEvent.getByLabel(_diTauPairsSrc, tauPairsInt);
 
@@ -48,20 +52,55 @@ void SVfitBlock::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   const reco::GenParticle* muon = 0;
   const reco::GenParticle* tauPos = 0;
   const reco::GenParticle* tauNeg = 0;
-  for (unsigned int j = 0; j < genParticles->size(); ++j) {
-    const reco::GenParticle& gen = (*genParticles)[j];
+  if (_verbosity) {
+    std::cout << setiosflags(std::ios::fixed);
+    std::cout << std::setprecision(2);
+    std::cout << "Status  pdgId  Energy      Pt       Eta     Phi  Charge" 
+              << std::endl;
+  }
+  for (reco::GenParticleCollection::const_iterator it  = genParticles->begin(); 
+                                                   it != genParticles->end(); ++it) {
+    const reco::GenParticle& gen = (*it);
     if (gen.status() != 3) continue;
 
+    if (_verbosity) 
+    std::cout << std::setw(6)  << gen.status() 
+              << std::setw(7)  << gen.pdgId()
+              << std::setw(8)  << gen.energy()
+              << std::setw(8)  << gen.pt()
+              << std::setw(10) << gen.eta()
+              << std::setw(8)  << gen.phi()
+              << std::setw(8)  << gen.charge()
+              << std::endl;
     int pdgid = abs(gen.pdgId());
     double chg = gen.charge();
-    if (pdgid == 13) { assert(!muon); muon = &gen; }
-    if (pdgid == 15 && chg < 0) { assert(!tauNeg); tauNeg = &gen; }
-    if (pdgid == 15 && chg > 0) { assert(!tauPos); tauPos = &gen; }
+
+    // Now collect the particles
+    if (pdgid == 13) {assert(!muon); muon = &gen;}
+    else if (pdgid == 15) {
+      bool motherIsW = false;
+      // Mother entry
+      for (reco::GenParticleCollection::const_iterator mit  = genParticles->begin(); 
+                                                       mit != genParticles->end(); ++mit) {
+        if (gen.mother() == &(*mit) ) {
+	  if (_verbosity) 
+          std::cout << "mother status: " << mit->status() 
+                    << ", pdgid: " << mit->pdgId()
+                    << std::endl;
+          if (abs(mit->pdgId()) == 24) {motherIsW = true; break;}
+        }
+      }
+      if (motherIsW) continue;
+      if (chg < 0) { assert(!tauNeg); tauNeg = &gen; }
+      else if (chg > 0) { assert(!tauPos); tauPos = &gen; }
+    }
   }
   if (!muon || !tauPos || !tauNeg) return;
 
-  for (unsigned i = 0 ; i < tauPairsInt->size() ; i++) {
-    const NSVfitEventHypothesisBase& tauPair = (*tauPairsInt)[i];
+  if (_verbosity) std::cout << "nTauPair = " << tauPairsInt->size() << std::endl;
+  for (NSVfitEventHypothesisByIntegrationCollection::const_iterator it  = tauPairsInt->begin(); 
+                                                                    it != tauPairsInt->end(); ++it) {
+    const NSVfitEventHypothesisBase& tauPair = (*it);
 
     const NSVfitResonanceHypothesisBase* svFitResonanceHypothesis = tauPair.NSVfitEventHypothesisBase::resonance("Higgs");
     if (!svFitResonanceHypothesis) continue;
@@ -79,8 +118,7 @@ void SVfitBlock::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     if (!svFitMuon) continue;
 
     if (fnSVDiTau == kMaxSVDiTau) {
-       edm::LogInfo("SVfitBlock") << "Too many SV DiTau, fnSVDiTau = " 
-                                  << fnSVDiTau; 
+       edm::LogInfo("SVfitBlock") << "Too many SV DiTau, fnSVDiTau = " << fnSVDiTau; 
        break;
     }
     svDiTauB = new ((*cloneSVDiTau)[fnSVDiTau++]) vhtm::SVDiTau();
@@ -96,6 +134,11 @@ void SVfitBlock::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     {
        svDiTauB->isGenMatched = true;
     }
+    if (_verbosity)
+    std::cout << "SVfitBlock mass:" << std::setw(9) << svDiTauB->mass 
+              << " isValidSolution: " << svDiTauB->isValidSolution 
+              << " isGenMatched: " << svDiTauB->isGenMatched 
+              << std::endl;
   }
 }
 #include "FWCore/Framework/interface/MakerMacros.h"
