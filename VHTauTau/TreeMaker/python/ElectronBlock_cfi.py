967c9bce4b0339915b5787033612fb899d1db4f0
sarkar <>
2012-05-27 22:18:31 +0000

    python/ElectronBlock_cfi.py

--- VHTauTau/TreeMaker/python/ElectronBlock_cfi.py
+++ VHTauTau/TreeMaker/python/ElectronBlock_cfi.py
@@ -5,10 +5,12 @@ from VHTauTau.TreeMaker.ElectronIDMVA_cfi import electronMVAIDNOIPcfg
 electronBlock = cms.EDAnalyzer("ElectronBlock",
     electronMVAIDNOIPcfg,
     verbosity     = cms.int32(0),
+    offlineBeamSpot = cms.InputTag('offlineBeamSpot'),
     trackSrc      = cms.InputTag('generalTracks'),
     dcsSrc        = cms.InputTag('scalersRawToDigi'),
     vertexSrc     = cms.InputTag('offlinePrimaryVerticesWithBS'),
-    electronSrc   = cms.InputTag('selectedPatElectron'),
+    convSrc       = cms.InputTag('allConversions'),
+    electronSrc   = cms.InputTag('selectedPatElectrons'),
     pfElectronSrc = cms.InputTag('particleFlow'),
     ecalEBInputTag = cms.InputTag('reducedEcalRecHitsEB'),
     ecalEEInputTag = cms.InputTag('reducedEcalRecHitsEE')
--- VHTauTau/TreeMaker/python/TauBlock_cfi.py
+++ VHTauTau/TreeMaker/python/TauBlock_cfi.py
@@ -1,8 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from VHTauTau.TreeMaker.AntiElectronIDMVA_cfi import antiElectronMVAIDcfg
 tauBlock = cms.EDAnalyzer("TauBlock",
-  antiElectronMVAIDcfg,
   verbosity = cms.int32(0),
   patTauSrc = cms.InputTag('selectedPatTaus'),
   vertexSrc = cms.InputTag('offlinePrimaryVerticesWithBS')
