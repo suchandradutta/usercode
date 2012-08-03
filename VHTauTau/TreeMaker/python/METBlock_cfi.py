e026bf308e137413ddcd739735fac4a839730c18
sarkar <>
2012-08-03 02:48:05 +0000

    METBlock update

--- VHTauTau/TreeMaker/python/METBlock_cfi.py
+++ VHTauTau/TreeMaker/python/METBlock_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 metBlock = cms.EDAnalyzer("METBlock",
   verbosity = cms.int32(0),
   metSrc    = cms.InputTag('patMETsPF'),
+  corrmetSrc = cms.InputTag('patPFMETsTypeIcorrected'),
   mvametSrc = cms.InputTag('patPFMetByMVA')  
 )
